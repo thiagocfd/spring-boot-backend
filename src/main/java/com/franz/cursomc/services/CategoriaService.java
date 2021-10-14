016ddcd9dcf52a1496b07392815f137510203cfc
Thiago Dourado <thiagocfdourado@gmail.com>
2021-10-14 14:12:03 -0300

    Correções: update em Categoriaservice e construtor de Pagamento

--- src/main/java/com/franz/cursomc/services/CategoriaService.java
+++ src/main/java/com/franz/cursomc/services/CategoriaService.java
@@ -32,10 +32,14 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 	public void delete(Integer id) {
