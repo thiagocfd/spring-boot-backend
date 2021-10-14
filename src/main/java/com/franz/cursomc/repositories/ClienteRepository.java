547ddceb0caa56ee7be32085cc0ddcff25f65717
Thiago Dourado <thiagocfdourado@gmail.com>
2021-10-14 16:37:04 -0300

    Email não repetido na inserção de Cliente

--- src/main/java/com/franz/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/franz/cursomc/repositories/ClienteRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.franz.cursomc.domain.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-	
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/franz/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/franz/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.franz.cursomc.services.validation;
 
+import com.franz.cursomc.domain.Cliente;
 import com.franz.cursomc.domain.enums.TipoCliente;
 import com.franz.cursomc.dto.ClienteNewDTO;
+import com.franz.cursomc.repositories.ClienteRepository;
 import com.franz.cursomc.resources.exceptions.FieldMessage;
 import com.franz.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidatorContext;
 
@@ -14,6 +17,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
